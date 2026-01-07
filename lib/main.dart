8b815d361269d4782de6c9d30a70c1aaffa2c8e2
nicos92 <nicolassandoval140692@gmail.com>
2026-01-07 05:36:42 -0300

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+// lib/main.dart temporal
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Estructura lista')),
+        body: Center(child: Text('¡Arquitectura en capas creada!')),
+      ),
+    )
+);
